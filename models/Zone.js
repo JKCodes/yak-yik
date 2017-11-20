fc0c797497a78c9eafa1a8b0e0a72c0c09561100
Joe Kim <josephkim0224@gmail.com>
2017-11-20 10:52:55 -0500

    create test form for comment creation

--- models/Zone.js
+++ models/Zone.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var ZoneSchema = new mongoose.Schema({
   name: {type:String, default:''},
   zipCodes: {type:Array, default:[]},
-  timestampt: {type:Date, default:Date.now}
+  timestamp: {type:Date, default:Date.now}
 });
 
 module.exports = mongoose.model('ZoneSchema', ZoneSchema);
