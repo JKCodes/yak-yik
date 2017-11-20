0e6006bad29bf081dfe460aab603d3787aef780b
Joe Kim <josephkim0224@gmail.com>
2017-11-20 10:21:58 -0500

    configure Comment and Zone controller

---
+++ controllers/ZoneController.js
@@ -0,0 +1,58 @@
+var Zone = require('../models/Zone')
+
+module.exports = {
+  find: function(params, callback) {
+    Zone.find(params, null, {sort: {timestamp: -1}}, function(err, zones) {
+      if (err) {
+        callback(err, null)
+        return
+      }
+
+      callback(null, zones)
+    })
+  },
+
+  findById: function(id, callback) {
+    Zone.findById(id, function(err, zone) {
+      if (err) {
+        callback(err, null)
+        return
+      }
+
+      callback(null, zone)
+    })
+  },
+
+  create: function(params, callback) {
+    Zone.create(params, function(err, zone) {
+      if (err) {
+        callback(err, null)
+        return
+      }
+
+      callback(null, zone)
+    })
+  },
+
+  update: function(id, params, callback) {
+    Zone.findByIdAndUpdate(id, params, {new:true},function(err, zone) {
+      if (err) {
+        callback(err, null)
+        return
+      }
+
+      callback(null, zone)
+    })
+  },
+
+  delete: function(id, callback) {
+    Zone.findByIdAndRemove(id, function(err) {
+      if (err) {
+        callback(err)
+        return
+      }
+
+      callback(null)
+    })
+  }
+}
