cd750f8a5938e4416ff61f4125cb4b767e7ec012
Joe Kim <josephkim0224@gmail.com>
2017-11-22 14:45:16 -0500

    create dummy data for comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -4,7 +4,13 @@ class Comments extends Component {
   render() {
     return (
       <div>
-        Comments
+        <h2>Comments: Zone 1</h2>
+        <ul>
+          <li>Comment 1</li>
+          <li>Comment 2</li>
+          <li>Comment 3</li>
+          <li>Comment 4</li>
+        </ul>
       </div>
     )
   }
