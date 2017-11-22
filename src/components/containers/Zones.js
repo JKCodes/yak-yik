38799dcda89f65515cf4dec24b204b9437ce72f6
Joe Kim <josephkim0224@gmail.com>
2017-11-22 15:51:15 -0500

    update zones component with dummy data and click and change handlers

--- src/components/containers/Zones.js
+++ src/components/containers/Zones.js
@@ -5,6 +5,10 @@ class Zones extends Component {
   constructor() {
     super()
     this.state = {
+      zone: {
+        name: '',
+        zipCode: ''
+      },
       list: [
         {name:'Zone 1', zipCode: '10012', numComments: 10},
         {name:'Zone 2', zipCode: '10013', numComments: 20},
@@ -15,6 +19,23 @@ class Zones extends Component {
     }
   }
 
+  updateZone(event) {
+    let updatedZone = Object.assign({}, this.state.zone)
+    updatedZone[event.target.id] = event.target.value
+    
+    this.setState({
+      zone: updatedZone
+    })
+  }
+
+  submitZone() {
+    let updatedList = Object.assign([], this.state.list)
+    updatedList.push(this.state.zone)
+    
+    this.setState({
+      list: updatedList  
+    })
+  }
 
   render() {
 
@@ -29,6 +50,11 @@ class Zones extends Component {
         <ol>
           {listItems}
         </ol>
+
+        <input id="name" onChange={this.updateZone.bind(this)} className="form-control" type="text" placeholder="Name" /><br />
+        <input id="zipCode" onChange={this.updateZone.bind(this)} className="form-control" type="text" placeholder="Zip Code" /><br />
+        <input id="numComments" onChange={this.updateZone.bind(this)} className="form-control" type="text" placeholder="Comments" /><br />
+        <button onClick={this.submitZone.bind(this)} className="btn btn-danger">Add Zone</button> 
       </div>
     )
   }
