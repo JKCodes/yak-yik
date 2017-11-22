4c6ffb02647619f62450488a96dd47c7469b2a3b
Joe Kim <josephkim0224@gmail.com>
2017-11-22 14:36:44 -0500

    create Home layout

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Zones from './Zones';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Zones />
+      </div>
+    )
+  }
+}
+
+export default Home;
