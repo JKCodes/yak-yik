1a1c15f94cf800c6233812321be7eaf4c179ac73
Joe Kim <josephkim0224@gmail.com>
2017-11-27 15:11:05 -0500

    add actions file for redux

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import constants from '../constants'
+
+export default {
+
+  zonesReceived: (zones) => {
+    return  {
+      type: constants.ZONES_RECEIVED,
+      zones: zones      
+    }
+  }
+}
