0921230b63b23d2588ff16c0f07df9cc4606af9e
Joe Kim <josephkim0224@gmail.com>
2017-11-28 14:42:34 -0500

    update login route

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,18 @@
 var express = require('express')
 var router = express.Router()
 
+router.get('/:action', function(req, res, next) {
+
+  var action = req.params.action
+  if (action == 'login') {
+
+    res.json({
+      confirmation: 'success',
+      action: action
+    })
+  }
+})
+
 router.post('/:action', function(req, res, next) {
 
   var action = req.params.action
