482a5f4fc45b9ca8daa97bfaaca8a8146dd0613a
Joe Kim <josephkim0224@gmail.com>
2017-11-28 11:30:50 -0500

    add zone key to comment schema

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 var CommentSchema = new mongoose.Schema({
+  zone: {type:String, default:''},
   username: {type:String, default:''},
   body: {type:String, default:''},
   timestamp: {type:Date, default:Date.now}
