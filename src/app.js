9b1c169ff6206b696648bf91bc58bf6375e85d09
Joe Kim <josephkim0224@gmail.com>
2017-11-28 17:08:15 -0500

    add react router configs

--- src/app.js
+++ src/app.js
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom';
 import Home from './components/layout/Home';
 import { Provider } from 'react-redux';
 import store from './stores';
+import { BrowserRouter as Router, Route, IndexRoute, Switch } from 'react-router-dom';
 
-class App extends Component {
+const app = (
+  <Provider store={store.configureStore() }>
+    <Router>
+      <Route path='/' component={Home}></Route>
+    </Router>
+  </Provider>
+)
 
-  render() {
-    return (
-      <Provider store={store.configureStore() }>
-        <div>
-          YakYik!
-          <Home />
-        </div>
-      </Provider>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
