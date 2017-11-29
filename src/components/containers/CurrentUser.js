71f6dc35695d224262b1b6caef3246ed9d57ff1d
Joe Kim <josephkim0224@gmail.com>
2017-11-29 12:03:31 -0500

    complete currentuser container's update profile details functionality

--- src/components/containers/CurrentUser.js
+++ src/components/containers/CurrentUser.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import actions from '../../actions'
 
 class CurrentUser extends Component {
+  constructor() {
+    super()
+    this.state = {
+      updated: {
+       
+      }
+    }
+  }
+
+  updateCurrentUser(event) {
+    event.preventDefault()
+    
+    let updatedProfile = Object.assign({}, this.state.updated)
+    updatedProfile[event.target.id] = event.target.value
+    this.setState({
+      updated: updatedProfile
+    })
+  }
+
+  updateProfile(event) {
+    event.preventDefault()
+
+    if (Object.keys(this.state.updated).length == 0) {
+      alert('No Changes Detected')
+      return
+    }
+
+    this.props.updateProfile(this.props.user, this.state.updated)
+  }
+
   render() {
+    const currentUser = this.props.user
+
     return (
       <div>
-        CurrentUser
+        <h2>Welcome { currentUser.username }</h2>
+        <input type="text" id="username" onChange={this.updateCurrentUser.bind(this)} defaultValue={currentUser.username} placeholder="Username" /><br />
+        <input type="text" id="gender" onChange={this.updateCurrentUser.bind(this)} defaultValue={currentUser.gender} placeholder="Gender" /><br />
+        <input type="text" id="city" onChange={this.updateCurrentUser.bind(this)} defaultValue={currentUser.city} placeholder="City" /><br />
+        <button onClick={this.updateProfile.bind(this)}>Update Profile</button>
       </div>
     )
   }
 }
 
-export default CurrentUser
+const stateToProps = (state) => {
+  return {
+    user: state.account.user
+  }
+}
+
+const dispatchToProps = (dispatch) => {
+  return {
+    updateProfile: (profile, updated) => dispatch(actions.updateProfile(profile, updated))
+  }
+}
+
+export default connect(stateToProps, dispatchToProps)(CurrentUser)
