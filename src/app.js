06727fc98bbc80ebe860708f1c05c7900bf580a5
Joe Kim <josephkim0224@gmail.com>
2017-11-28 20:25:45 -0500

    configure app.js to use the router

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { Home, ProfileInfo } from './components/layout';
 import { Provider } from 'react-redux';
 import store from './stores';
-import { BrowserRouter as Router, Route, IndexRoute, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const app = (
   <Provider store={store.configureStore() }>
     <Router>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/profile/:username' component={ProfileInfo} />
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/profile/:username' component={ProfileInfo}></Route>
       </Switch>
     </Router>
   </Provider>
